c7283e73f4bc260ffc364ab45d424adb6a1f835b
Kelly Colht <colhk@seattleu.edu>
2018-08-10 09:42:35 -0700

    testing weather datat retrieval

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -2,27 +2,32 @@
   <div class="form-group col-lg-3 col-md-12 col-sm-12">
     <div class="card mb-5">
       <h3 class="card-header mb-3">Weather (soon to be)</h3>
-          
-      <ul>
+        <ul>
+          <li>lat: {{ lat }}</li>
+          <li>long: {{ long }}</li>
+          <li>humidity: {{ results.main.humidity }}</li>
+          <li>temp: {{ results.main.temp }}</li>
+          <li>test: {{ results }}</li>
 
-          <h2>lat: {{ lat }}</h2>
-          <h2>long: {{ long }}</h2>
+          <li>test: {{ results.weather }}</li>
+
+          <li>description: {{ results.weather['description'] }}</li>
           <!-- <img v-bind:src="'http://openweathermap.org/img/w/' + weatherSummary.icon + '.png'" v-bind:alt="weatherSummary.main"> -->
-
-      </ul>
-  
+      </ul> 
     </div>
     </div>
   
   </template>
 
 <script>
+import {API} from '@/common/api';
 
 export default {
   name: 'weather',
   data(){
-    return {
-        
+    return {         
+      results: null,
+      errors: [],
     }
   },
   props: ['info'],
@@ -31,12 +36,35 @@ export default {
   // FIX: [Vue warn]: Error in render: "TypeError: this.info.coordinate_location is undefined"
   computed: {
     lat () {
-      return this.info.coordinate_location.replace(/[A-Z][a-z]*/,"").replace(/\(|\)/g, "").split(' ')[1];
+      return this.info.coordinate_location.replace(/[A-Z][a-z]*/,"").replace(/\(|\)/g, "").split(' ')[1].replace(/\..*/, "");
       },
     long () {
-      return this.info.coordinate_location.replace(/[A-Z][a-z]*/,"").replace(/\(|\)/g, "").split(' ')[0];
+      return this.info.coordinate_location.replace(/[A-Z][a-z]*/,"").replace(/\(|\)/g, "").split(' ')[0].replace(/\..*/, "");
       }
-}
+},
+methods: {
+    getWeather: function () {
+      console.log('Getting weather...');
+      API.get('weather', {
+        params: {
+          lat: this.lat,
+          lon: this.long
+        }
+      })
+      .then(response => {
+        this.results = response.data
+        console.log(response);
+      })
+      .catch(error => {
+        this.errors.push(error.message)
+        console.log(error);
+      });
+    }
+  },
+  beforeMount () {
+    this.getWeather();
+  }
+
 }
 
 </script>
@@ -60,3 +88,8 @@ ul {
 </style>
 
 
+// works
+// http://api.openweathermap.org/data/2.5/weather?lat=3&lon=-141&APPID=8e5eadaad6674b9ea1e7e66f59794d34&units=imperial
+
+// doesn't work
+// http://api.openweathermap.org/data/2.5/weather?q=lat=3&lon=-141&APPID=8e5eadaad6674b9ea1e7e66f59794d34&units=imperial
